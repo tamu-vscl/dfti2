aa115238b522d20215e3fbbc11e745bbf0ce7bd6
Kameron Eves <KameronEves@gmail.com>
2021-02-16 19:17:53 -0600

    signal injection compiling

--- include/dfti2/auto_excitation.h
+++ include/dfti2/auto_excitation.h
@@ -3,11 +3,12 @@
 
 #define SIGNAL_TYPE_OFFSET 0
 #define SIGNAL_TYPE_SQUARE 1
-#define SIGNAL_TYPE_RAMP 2
+#define SIGNAL_TYPE_SAWTOOTH 2
 #define SIGNAL_TYPE_SINE 3
 #define SIGNAL_TYPE_SINE_SWEEP 4
 
 #define PI 3.14159
+#define IGNORE_CHANNEL UINT16_MAX
 
 #include <ros/ros.h>
 
@@ -21,23 +22,23 @@ public:
 
   struct Signal
   {
-    int type = SIGNAL_TYPE_OFFSET;
-    double period = 1;
-    double amplitude = 0;
-    double offset = 0;
-    double last_update = -1;
-    double percent_of_period = 0;
-    double period_count = 0;
-    double length = 1;
-    double value = 0;
-    double settling_time = 5;
-    double update_rate = 100;
-    double f0 = 1.0;
-    double f1 = 10.0;
+    // Signal parameters
+    int type = IGNORE_CHANNEL; // Type of signal
+    double period = 1; // Length of 1 period in seconds.
+    double amplitude = 0; // Scaling factor for signal (exact implimentation varies between signal types) TODO: Units
+    double offset = 1500; // Moves the signal up or down to center it around a value other then 0
+    double f0 = 1.0; // Intial frequency for sine sweep
+    double f1 = 10.0; // Final frequency for sine sweep
+    double delay = 0; // Seconds before this signal begins
+
+    // Logistical parameters
+    double percent_of_period = 0; // Tracks where in the period the signal is curently
+    double value = UINT16_MAX; // Actual ouput of the signal generator
+    double length = 1; // Number of periods to execute. Can be partial period(s)
   };
 
   AutoExcitation();
-  inline const Signal& signal() const { return signal_; }
+  inline const Signal& signal(int i) const { return signal_[i]; }
 
 private:
 
@@ -45,15 +46,17 @@ private:
   ros::Publisher offboard_override_pub_;
   ros::ServiceServer arm_disturb_input_svc_;
 
-  bool arm_disturb_input_callback(rosflight_msgs::DisturbInput::Request  &req,
-                                  rosflight_msgs::DisturbInput::Response &res);
-  void update_signal();
+  bool arm_disturb_input_callback(dfti2::DisturbInput::Request  &req,
+                                  dfti2::DisturbInput::Response &res);
+  void update_signals();
+  void update_signal(int i);
   void load_param();
 
   Signal signal_ [8];
   double arm_time_out_time_;
-  int output_type_;
   int trigger_channel_;
+  double update_rate_;
+  double signal_start_time_;
 };
 
 #endif // AUTO_EXCITATION_H
--- src/auto_excitation.cpp
+++ src/auto_excitation.cpp
@@ -35,6 +35,7 @@ rosflight board. This is useful, among other things, for system identification
 */
 
 #include <cmath>
+#include <vector>
 
 #include <ros/ros.h>
 
@@ -49,7 +50,7 @@ int main(int argc, char **argv)
 
 AutoExcitation::AutoExcitation()
 {
-  offboard_override_pub_ = nh_.advertise<rosflight_msgs::AuxCommand>("/mavros/rc/override",1);
+  offboard_override_pub_ = nh_.advertise<mavros_msgs::OverrideRCIn>("/mavros/rc/override",1);
 
   load_param();
 
@@ -63,116 +64,128 @@ bool AutoExcitation::arm_disturb_input_callback(dfti2::DisturbInput::Request  &r
 
   load_param();
 
-  if(dir_svc.response.exists)
+  ROS_WARN("Disturbance armed!");
+  double arm_time = ros::Time::now().toSec();
+  ros::Rate loop_rate(10);
+  boost::shared_ptr<mavros_msgs::RCIn const> rc_raw_msg;
+  while (1)
   {
-    ROS_WARN("Channel %i disturbance armed!",req.channel);
-    signal_.last_update = ros::Time::now().toSec();
-    while (1)
+    rc_raw_msg = ros::topic::waitForMessage<mavros_msgs::RCIn>("/mavros/rc/in");
+    if(rc_raw_msg->channels[trigger_channel_]>1900)
     {
-      boost::shared_ptr<mavros_msgs::RCIn const> rc_raw_msg = ros::topic::waitForMessage<mavros_msgs::RCIn>("/mavros/rc/in");
-      if(rc_raw_msg->channels[trigger_channel_]>1900)
-      {
-        ROS_WARN("Initiating disturbance on channel %i",req.channel);
-        break;
-      }
-
-      if ((ros::Time::now().toSec()-signal_.last_update)>=arm_time_out_time_)
-      {
-        ROS_INFO("The disturbance arm has timed out.");
-        signal_ = Signal();
-        return res.finished;
-      }
+      ROS_WARN("Initiating disturbance");
+      break;
     }
-  }
-  else
-  {
-    ROS_ERROR("Unable to arm disturbance. Check that the trigger channel is set correctly on transmitter.");
-    signal_ = Signal();
-    return res.finished;
-  }
 
-  ros::Rate loop_rate(signal_.update_rate);
-  signal_.last_update = ros::Time::now().toSec();
-  while (!res.finished)
-  {
-    update_signal();
-    mavros_msgs::OverrideRCIn msg;
-    for (int i = 0;i<8;i++)
+    if ((ros::Time::now().toSec()-arm_time)>=arm_time_out_time_)
     {
-      msg.type_array[i] = UINT16_MAX;
-      msg.values[i] = 0.0;
+      ROS_INFO("The disturbance arm has timed out.");
+      return res.finished;
     }
-    msg.type_array[req.channel] = output_type_;
-    msg.values[req.channel] = signal_.value;
-    offboard_override_pub_.publish(msg);
-    // TODO: include servo vs motor adjustment
-    // TODO: add multi input
-    res.finished = signal_.length <= (signal_.period_count+signal_.percent_of_period);
+
     loop_rate.sleep();
   }
 
-  signal_ = Signal();
+  loop_rate = ros::Rate(update_rate_);
+  signal_start_time_ = ros::Time::now().toSec(); // TODO: remove from signal
+  while (!res.finished)
+  {
+    update_signals();
+
+    mavros_msgs::OverrideRCIn msg;
+    for (int i = 0;i<8;i++)
+    {
+      msg.channels[i] = signal_[i].value;
+    }
+
+    offboard_override_pub_.publish(msg);
+    // TODO: add multi input
+
+    bool not_done = true;
+    for(int i = 0;i<8;i++){not_done &= (signal_[i].percent_of_period < signal_[i].length);}
+    res.finished = !not_done;
+
+    rc_raw_msg = ros::topic::waitForMessage<mavros_msgs::RCIn>("/mavros/rc/in");
+    if(rc_raw_msg->channels[trigger_channel_]<=1900){break;}
+
+    loop_rate.sleep();
+  }
 
   return res.finished;
 }
 
-void AutoExcitation::update_signal()
+void AutoExcitation::update_signals()
 {
-  double current_time = ros::Time::now().toSec();
-  float step = current_time - signal_.last_update;
-  signal_.percent_of_period = step/signal_.period;
-  if (floor(signal_.percent_of_period)!=0)
+  for(int i = 0;i<8;i++)
   {
-    signal_.percent_of_period-=floor(signal_.percent_of_period);
-    signal_.period_count++;
-    signal_.last_update = current_time;
+    signal_[i].percent_of_period = (ros::Time::now().toSec() - (signal_start_time_ + signal_[i].delay))/signal_[i].period;
+    if(0<=signal_[i].percent_of_period && signal_[i].percent_of_period<=signal_[i].length){update_signal(i);}
+    else{signal_[i].value = IGNORE_CHANNEL;}
   }
+}
 
-  switch(signal_.type)
+void AutoExcitation::update_signal(int i)
+{
+  switch(signal_[i].type)
   {
   case SIGNAL_TYPE_OFFSET:
-    signal_.value = signal_.offset;
+    signal_[i].value = signal_[i].offset;
     break;
   case SIGNAL_TYPE_SQUARE: // Note that the square wave and ramp offset is bassed off of the zero point. i.e. an offset of 0 will cause the wave to oscilate between 0 and 1 (if the amplitude is 1).
-    signal_.value = signal_.percent_of_period>=0.5 ? signal_.amplitude+signal_.offset : signal_.offset;
+    signal_[i].value = floor(signal_[i].percent_of_period)>=0.5 ? signal_[i].amplitude+signal_[i].offset : signal_[i].offset;
     break;
-  case SIGNAL_TYPE_RAMP: // Note that the square wave and ramp offset is bassed off of the zero point. i.e. an offset of 0 will cause the wave to oscilate between 0 and 1 (if the amplitude is 1).
-    signal_.value = signal_.amplitude*signal_.percent_of_period + signal_.offset;
+  case SIGNAL_TYPE_SAWTOOTH: // Note that the square wave and ramp offset is bassed off of the zero point. i.e. an offset of 0 will cause the wave to oscilate between 0 and 1 (if the amplitude is 1).
+    signal_[i].value = signal_[i].amplitude*floor(signal_[i].percent_of_period) + signal_[i].offset;
     break;
   case SIGNAL_TYPE_SINE: // Note that the sin wave offset is bassed off of the middle point. i.e. an offset of 0 will cause the wave to oscilate between -1 and 1 (if the amplitude is 1).
-    signal_.value = signal_.amplitude*sin(signal_.percent_of_period*2*PI) + signal_.offset;
+    signal_[i].value = signal_[i].amplitude*sin(signal_[i].percent_of_period*2*PI) + signal_[i].offset;
     break;
-  case SIGNA_TYPE_SINE_SWEEP:
-    signal_.value = signal_.amplitude*sin(2*PI*signal_.f0*signal_.period*(pow(signal_.f1/signal_.f0,signal_.percent_of_period)-1)/(log(signal_.f1)-log(signal_.f0)) + signal_.offset;
+  case SIGNAL_TYPE_SINE_SWEEP:
+    signal_[i].value = signal_[i].amplitude*sin(2*PI*((signal_[i].f1-signal_[i].f0)/2*pow(signal_[i].percent_of_period,2)*signal_[i].period + signal_[i].f0*signal_[i].percent_of_period*signal_[i].period)) + signal_[i].offset;
     break;
   default: // This tells the autopilot to ignore this signal and the RC signal is used instead.
-    signal_.value = UINT16_MAX;
+    signal_[i].value = IGNORE_CHANNEL;
     break;
   }
 }
 
 void AutoExcitation::load_param()
 {
+  std::vector<int> type;
+  std::vector<double> period;
+  std::vector<double> amplitude;
+  std::vector<double> offset;
+  std::vector<double> f0;
+  std::vector<double> f1;
+  std::vector<double> delay;
+
+  std::vector<double> length;
+
   // load parameters
-  nh_.param("type",signal_.type,0);
-  if(nh_.hasParam("period"))
+  nh_.getParam("type",type);
+  nh_.getParam("period",period);
+  nh_.getParam("amplitude",amplitude);
+  nh_.getParam("offset",offset);
+  nh_.getParam("f0",f1);
+  nh_.getParam("f1",f0);
+  nh_.getParam("delay",delay);
+  nh_.getParam("length",length);
+
+  for(int i = 0;i<8;i++)
   {
-    nh_.param("period",signal_.period,1.0);
+    signal_[i].type = type[i];
+    signal_[i].period = period[i];
+    signal_[i].amplitude = amplitude[i];
+    signal_[i].offset = offset[i];
+    signal_[i].f0 = f0[i];
+    signal_[i].f1 = f1[i];
+    signal_[i].delay = delay[i];
+    signal_[i].percent_of_period = 0;
+    signal_[i].value = IGNORE_CHANNEL;
+    signal_[i].length = length[i];
   }
-  else
-  {
-    float frequency;
-    nh_.getParam("f0",frequency);
-    signal_.period = 1.0/frequency;
-  }
-  nh_.param("amplitude",signal_.amplitude,0.0);
-  nh_.param("offset",signal_.offset,0.0);
-  nh_.param("length",signal_.length,1.0);
-  nh_.param("settling_time",signal_.settling_time,5.0);
-  nh_.param("arm_time_out_time",arm_time_out_time_,10.0);
-  nh_.param("update_rate",signal_.update_rate,100.0);
-  nh_.param("f0",signal_.f1,1.0);
-  nh_.param("f1",signal_.f0,10.0);
-  nh_.param("output_type",output_type_,2);
-  nh_.param("trigger_channel",trigger_channel_,9);
+
+  nh_.getParam("arm_time_out_time",arm_time_out_time_);
+  nh_.getParam("trigger_channel",trigger_channel_);
+  nh_.getParam("update_rate",update_rate_);
 }
