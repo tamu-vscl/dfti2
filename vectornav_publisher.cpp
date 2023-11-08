2a2b5082b89e2375aa58b342ae98eac322cdc1df
Ms-Laura-Escamilla <144632344+Ms-Laura-Escamilla@users.noreply.github.com>
2023-11-08 04:32:23 -0600

    Add files via upload
    added u, v, w, lat, long, alt

---
+++ vectornav_publisher.cpp
@@ -0,0 +1,68 @@
+#include "ros/ros.h"
+#include "dfti2/dftiData.h"
+#include "nav_msgs/Odometry.h"
+#include <vectornav/Ins.h>
+#include <vector>
+
+class vectornav_publisher_node
+{
+  public:
+    vectornav_publisher_node();
+  private:
+    ros::NodeHandle n;
+    ros::Subscriber ins_sub;
+    ros::Publisher data_pub;
+
+    void insCallback(const vectornav::Ins::ConstPtr& in_msg);
+};
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "vectornav_publisher");
+  vectornav_publisher_node start;
+  ros::spin();
+  return 0;
+}
+
+vectornav_publisher_node::vectornav_publisher_node()
+{
+  data_pub = n.advertise<dfti2::dftiData>("dfti_data",1000);
+  ins_sub = n.subscribe("/vectornav/INS",1000,&vectornav_publisher_node::insCallback,this);
+}
+
+void vectornav_publisher_node::insCallback(const vectornav::Ins::ConstPtr& in_msg)
+{
+  dfti2::dftiData out_msg;
+  out_msg.header = in_msg->header;
+
+  out_msg.type = "vn_psi";  // default to NED unless changed in the vectornav param file
+  out_msg.data = in_msg->yaw;
+  data_pub.publish(out_msg);
+  out_msg.type = "vn_theta";
+  out_msg.data = in_msg->pitch;
+  data_pub.publish(out_msg);
+  out_msg.type = "vn_phi";
+  out_msg.data = -in_msg->roll;
+  data_pub.publish(out_msg);
+
+  out_msg.type = "vn_lat";  // default to NED unless changed in the vectornav param file
+  out_msg.data = in_msg->latitude;
+  data_pub.publish(out_msg);
+  out_msg.type = "vn_long";
+  out_msg.data = in_msg->longitude;
+  data_pub.publish(out_msg);
+  out_msg.type = "vn_alt";
+  out_msg.data = -in_msg->altitude;
+  data_pub.publish(out_msg);
+
+  out_msg.type = "vn_u";  // default to NED unless changed in the vectornav param file
+  out_msg.data = in_msg->velN;
+  data_pub.publish(out_msg);
+  out_msg.type = "vn_v";
+  out_msg.data = in_msg->velE;
+  data_pub.publish(out_msg);
+  out_msg.type = "vn_w";
+  out_msg.data = -in_msg->velD;
+  data_pub.publish(out_msg);
+
+}
